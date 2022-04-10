005f9327d913a19ce2bf7271adb73eeaacd8f4d2
AntonioSabella <asabella6@gmail.com>
2022-04-10 17:12:36 +0200

    Add JS adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 /* Simon says
- Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta,
+i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, 
+il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 /* Passaggi:
 - div nell'html
@@ -35,7 +37,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
  }
  console.log(randomNumbers);
 
- //4. 
+ //4. Stampare a schermo i numeri che l'utente dovrà memorizzare
  numberElement.innerHTML = `Memorizza questi numeri: ${randomNumbers.toString()}`
 
  // Faccio partire un timer di 30 secondi
